b821e53f807ffd38ea11dfa2adc8ef003810db94
jerryfly <1113821597@qq.com>
2021-06-16 16:55:52 +0800

    增加下发任务路由

--- router/router.go
+++ router/router.go
@@ -13,7 +13,7 @@ func InitRouter() (r *gin.Engine) {
 	r.Use(middleware.LoggerHandler, middleware.RecoverHandler)
 
 	r.GET("/ping", handlers.PingPong)
-
+	r.POST("/seal/seal_commit_phase2", handlers.SealCommitPhase2)
 
 	return
 }
