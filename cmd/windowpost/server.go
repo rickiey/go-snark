a2912c44bc354c7ca4b44268b17aa81d80fe56e6
jerryfly <1113821597@qq.com>
2021-07-23 16:32:22 +0800

    优化文件

---
+++ cmd/windowpost/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"errors"
+	pb "go-snark/cmd/windowpost/proto"
+
+	"github.com/golang/glog"
+)
+
+type SnarkServer struct {
+	pb.UnimplementedWindowGrpcServer
+}
+
+func NewSnarkServer() *SnarkServer {
+	return &SnarkServer{}
+}
+
+// AllocateTask 接收计算任务
+func (s *SnarkServer) AllocateTask(ctx context.Context, task *pb.TaskRequest) (*pb.TaskResponse, error) {
+	glog.Info("receive task: ", task.MinerID)
+	return nil, nil
+}
+
+// HeartBeat 接收心跳检测
+func (s *SnarkServer) HeartBeat(ctx context.Context, req *pb.HeartBeatRequest) (*pb.HeartBeatResponse, error) {
+	glog.Info("receive heart beat: ", req.SentTime)
+	return nil, nil
+}
+
+// RegisterWorker snark server 端无需实现
+func (s *SnarkServer) RegisterWorker(ctx context.Context, req *pb.WindowWorkerRequest) (*pb.WindowWorkerResponse, error) {
+	return nil, errors.New("unimplements")
+}
