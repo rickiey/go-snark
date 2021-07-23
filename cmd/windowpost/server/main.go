96967dc1b79fa3aa4d0322bcead09a82bbb93339
jerryfly <1113821597@qq.com>
2021-07-23 16:10:19 +0800

    window snark

---
+++ cmd/windowpost/server/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"flag"
+	pb "go-snark/cmd/windowpost"
+	"net"
+
+	"github.com/golang/glog"
+	"google.golang.org/grpc"
+)
+
+var LocalAddr = flag.String("local_addr", "", "the local addr:port")
+var RemoteAddr = flag.String("remote_addr", "", "remote server addr:port")
+
+/**
+snark （每台挂载存储，2/4张卡，对应2/4个进程，端口区分，各为一个worker）
+	server: 1. 接收任务进行计算，并返回结果
+	client: 1. 向window机注册snark server
+window机
+	server: 1. 管理client
+			2. 对client做心跳，当前可用数不足，则电话告警
+	client: 1. 调用snark server端分配任务，并接收计算结果， 1个partition，一个client
+
+
+*/
+
+func main() {
+	listener, err := net.Listen("tcp", *LocalAddr) // 监听本地端口
+	if err != nil {
+		panic(err)
+	}
+	glog.Info("grpc server Listing on", *LocalAddr)
+
+	grpcServer := grpc.NewServer() // 新建gRPC服务器实例
+
+	client := NewSnarkClient(*RemoteAddr)
+	status, err := client.RegisterWorker(context.Background(), *LocalAddr)
+
+	if nil != err {
+		panic(err)
+	}
+
+	if status != 1 {
+		panic("register failed")
+	}
+
+	glog.Info("register success")
+
+	server := NewSnarkServer()
+
+	pb.RegisterWindowGrpcServer(grpcServer, server)
+
+	if err = grpcServer.Serve(listener); err != nil { //用服务器 Serve() 方法以及我们的端口信息区实现阻塞等待，直到进程被杀死或者 Stop() 被调用
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -12,4 +12,10 @@ require (
 	github.com/gin-gonic/gin v1.7.2
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/golang/glog v0.0.0-20210429001901-424d2337a529
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	google.golang.org/genproto v0.0.0-20210715145939-324b959e9c22 // indirect
+	google.golang.org/grpc v1.39.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 )
